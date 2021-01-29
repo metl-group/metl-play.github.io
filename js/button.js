6794ebd34ca156c4e5f19da9e16f50608a54fe08
metl-play <68977058+metl-play@users.noreply.github.com>
2021-01-29 13:42:37 +0100

    Project

--- js/button.js
+++ js/button.js
@@ -4,9 +4,23 @@ let buttonw = document.getElementById('buttonw');
 buttond.addEventListener('click', function(){
     html.style.background = '#36393f';
     html.style.color = 'white';
+    titel.style.color = '#e2e2e2';
+    by.style.color = '#e2e2e2';
+    nav.style.background = 'purple';
+    navul1.style.background = 'purple';
+    navul2.style.background = 'purple';
+    conhead.style.color = '#e2e2e2';
+    content.style.color = '#e2e2e2';
 })
 
 buttonw.addEventListener('click', function(){
     html.style.background = 'white';
     html.style.color = '#36393f';
+    titel.style.color = '#2d2d2d';
+    by.style.color = '#2d2d2d';
+    nav.style.background = '#d300d3';
+    navul1.style.background = '#d300d3';
+    navul2.style.background = '#d300d3';
+    conhead.style.color = '#2d2d2d';
+    content.style.color = '#2d2d2d';
 })
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(e) {
+
+        $('nav ul li').stop().hover(
+            function(e) {
+                $(this).children('ul').stop().slideDown(500);
+            },
+            function(e) {
+                $(this).children('ul').stop().slideUp(500);
+            }
+        );
+});
