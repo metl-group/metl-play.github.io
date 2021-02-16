1e042750735815aeff0a4148ab81a7d30114e69d
metl-play <68977058+metl-play@users.noreply.github.com>
2021-02-16 23:01:51 +0100

    Update

--- js/button.js
+++ js/button.js
@@ -6,9 +6,6 @@ d.addEventListener('click', function(){
     html.style.background = '#36393f';
     html.style.color = '#2d2d2d';
 
-    btnd.style.background = '#800080';
-    btnw.style.background = '#800080';
-
     titel.style.color = '#e2e2e2';
     by.style.color = '#e2e2e2';
 
@@ -17,19 +14,20 @@ d.addEventListener('click', function(){
     navul1.style.background = '#800080';
     navul2.style.background = '#800080';
 
-    conhead.style.color = '#e2e2e2';
-    con.style.color = '#e2e2e2';
 
+    conhead.style.color = '#e2e2e2';
     conlwrhead.style.color = '#e2e2e2';
+    con.style.color = '#e2e2e2';
     conlwr.style.color = '#e2e2e2';
+
+    btnd.style.background = '#800080';
+    btnw.style.background = '#800080';
+    btnrgb.style.background = '#800080';
 })
 
 w.addEventListener('click', function(){
     html.style.background = '#e2e2e2';
     html.style.color = '#36393f';
-
-    btnd.style.background = '#d300d3';
-    btnw.style.background = '#d300d3';
 
     titel.style.color = '#2d2d2d';
     by.style.color = '#2d2d2d';
@@ -39,11 +37,15 @@ w.addEventListener('click', function(){
     navul1.style.background = '#d300d3';
     navul2.style.background = '#d300d3';
 
-    conhead.style.color = '#2d2d2d';
-    con.style.color = '#2d2d2d';
 
+    conhead.style.color = '#2d2d2d';
     conlwrhead.style.color = '#2d2d2d';
+    con.style.color = '#2d2d2d';
     conlwr.style.color = '#2d2d2d';
+
+    btnd.style.background = '#d300d3';
+    btnw.style.background = '#d300d3';
+    btnrgb.style.background = '#d300d3';
 })
 
 rgb.addEventListener('click', function(){
--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,10 @@ $(document).ready(function(e) {
 
         $('nav ul li').stop().hover(
             function(e) {
-                $(this).children('ul').stop().slideDown(250);
+                $(this).children('ul').stop().slideDown(500);
             },
             function(e) {
-                $(this).children('ul').stop().slideUp(250);
+                $(this).children('ul').stop().slideUp(500);
             }
         );
 });
