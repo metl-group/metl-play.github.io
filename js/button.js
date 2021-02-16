9778236ff0861444a2c445648c84e77ce5d7e4dc
metl-play <davidundivan.99@gmail.com>
2021-02-16 15:52:41 +0100

    Impressum- Fixed issue by changing design in kontakt.html

--- js/button.js
+++ js/button.js
@@ -6,6 +6,9 @@ d.addEventListener('click', function(){
     html.style.background = '#36393f';
     html.style.color = '#2d2d2d';
 
+    btnd.style.background = '#800080';
+    btnw.style.background = '#800080';
+
     titel.style.color = '#e2e2e2';
     by.style.color = '#e2e2e2';
 
@@ -14,20 +17,19 @@ d.addEventListener('click', function(){
     navul1.style.background = '#800080';
     navul2.style.background = '#800080';
 
-
     conhead.style.color = '#e2e2e2';
-    conlwrhead.style.color = '#e2e2e2';
     con.style.color = '#e2e2e2';
-    conlwr.style.color = '#e2e2e2';
 
-    btnd.style.background = '#800080';
-    btnw.style.background = '#800080';
-    btnrgb.style.background = '#800080';
+    conlwrhead.style.color = '#e2e2e2';
+    conlwr.style.color = '#e2e2e2';
 })
 
 w.addEventListener('click', function(){
     html.style.background = '#e2e2e2';
     html.style.color = '#36393f';
+
+    btnd.style.background = '#d300d3';
+    btnw.style.background = '#d300d3';
 
     titel.style.color = '#2d2d2d';
     by.style.color = '#2d2d2d';
@@ -37,15 +39,11 @@ w.addEventListener('click', function(){
     navul1.style.background = '#d300d3';
     navul2.style.background = '#d300d3';
 
-
     conhead.style.color = '#2d2d2d';
-    conlwrhead.style.color = '#2d2d2d';
     con.style.color = '#2d2d2d';
-    conlwr.style.color = '#2d2d2d';
 
-    btnd.style.background = '#d300d3';
-    btnw.style.background = '#d300d3';
-    btnrgb.style.background = '#d300d3';
+    conlwrhead.style.color = '#2d2d2d';
+    conlwr.style.color = '#2d2d2d';
 })
 
 rgb.addEventListener('click', function(){
