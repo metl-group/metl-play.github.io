655841bccd76f55fad016470d0b4902433d7c098
metl-play <davidundivan.99@gmail.com>
2021-02-17 14:50:19 +0100

    Update

--- js/button.js
+++ js/button.js
@@ -9,20 +9,19 @@ d.addEventListener('click', function(){
     titel.style.color = '#e2e2e2';
     by.style.color = '#e2e2e2';
 
+    btnd.style.background = '#800080';
+    btnw.style.background = '#800080';
+
     nav.style.background = '#800080';
     navul0.style.background = '#800080';
     navul1.style.background = '#800080';
     navul2.style.background = '#800080';
 
-
     conhead.style.color = '#e2e2e2';
-    conlwrhead.style.color = '#e2e2e2';
     con.style.color = '#e2e2e2';
-    conlwr.style.color = '#e2e2e2';
 
-    btnd.style.background = '#800080';
-    btnw.style.background = '#800080';
-    btnrgb.style.background = '#800080';
+    conlwrhead.style.color = '#e2e2e2';
+    conlwr.style.color = '#e2e2e2';
 })
 
 w.addEventListener('click', function(){
@@ -32,20 +31,19 @@ w.addEventListener('click', function(){
     titel.style.color = '#2d2d2d';
     by.style.color = '#2d2d2d';
 
+    btnd.style.background = '#d300d3';
+    btnw.style.background = '#d300d3';
+
     nav.style.background = '#d300d3';
     navul0.style.background = '#d300d3';
     navul1.style.background = '#d300d3';
     navul2.style.background = '#d300d3';
 
-
     conhead.style.color = '#2d2d2d';
-    conlwrhead.style.color = '#2d2d2d';
     con.style.color = '#2d2d2d';
-    conlwr.style.color = '#2d2d2d';
 
-    btnd.style.background = '#d300d3';
-    btnw.style.background = '#d300d3';
-    btnrgb.style.background = '#d300d3';
+    conlwrhead.style.color = '#2d2d2d';
+    conlwr.style.color = '#2d2d2d';
 })
 
 rgb.addEventListener('click', function(){
