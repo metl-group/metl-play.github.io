63c044ab33a2cbd3af36a999c60bac846b4f6409
metl-play <davidundivan.99@gmail.com>
2021-02-18 15:55:22 +0100

    Updated Game code

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ function lemath()
 {
     count++;
     var counter = document.getElementById('counter');
-    counter.innerHTML = "Counter: " + count;
+    counter.innerHTML = "Counter: " + (count -1);
 }
 
 function begin()
@@ -43,7 +43,7 @@ var checkDead = setInterval(function(){
         block.style.display = "none";
         block1.style.display = "none";
         block2.style.display = "none";
-        alert("u lose. Score: " + count);
+        alert("u lose. Score: " + (count -1));
         count = "none";
     }
 },10);
