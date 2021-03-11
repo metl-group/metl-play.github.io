bc89435efb731743a49ce99afb96addaeb8937f9
metl-play <68977058+metl-play@users.noreply.github.com>
2021-03-12 00:51:11 +0100

    Update game

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ function begin(){
     t=setTimeout(begin, 950, window);
 }
 
-document.addEventListener('DOMContentLoaded' && setTimeout(10000), function (){
+document.addEventListener('DOMContentLoaded' && t=5, function (){
     begin();
 });
 
